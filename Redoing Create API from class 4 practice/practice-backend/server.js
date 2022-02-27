e1a1e07b057b79d7bb411fd565cc71c47bf2fb50
Angel Valentin <angelgvalentin7@gmail.com>
2022-02-27 07:19:07 -0600

    added section notes

--- Redoing Create API from class 4 practice/practice-backend/server.js
+++ Redoing Create API from class 4 practice/practice-backend/server.js
@@ -4,8 +4,10 @@ const cors = require("cors");
 const app = express();
 const todosController = require("./controllers/todos.js");
 
+/* -------------------------------------------------------------------------- */
+/*                                 MIDDLEWARE                                 */
+/* -------------------------------------------------------------------------- */
 app.use(cors());
-
 app.use(express.json());
 app.use("/todos", todosController);
 
@@ -13,6 +15,9 @@ app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
+/* -------------------------------------------------------------------------- */
+/*                           CONNECTING TO DATABASE                           */
+/* -------------------------------------------------------------------------- */
 mongoose.connect("mongodb://localhost:27017/APIpracticecrud");
 mongoose.connection.once("open", () => {
   console.log("connected to mongod...");
