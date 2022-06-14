1436e7b529d37977c592bc64c606b8ef6181ff56
Angel Valentin <angelgvalentin7@gmail.com>
2022-06-14 11:25:15 -0500

    Added method to reverse string completely manually without using StringBuilder at all.

--- java-reverse-string/ReverseStringRunner.java
+++ java-reverse-string/ReverseStringRunner.java
@@ -8,10 +8,14 @@ public class ReverseStringRunner {
 
         String str = "Hello World";
 //        System.out.println(reversedWithStringBuilder(str));
-        System.out.println(reversedManually((str)));
+//        System.out.println(reversedManually((str)));
+
+        System.out.println(reverseStringManuallyWithoutStringBuilder((str)));
 
 
     }
+
+        //Using the StringBuilder() API we don't have to create new instances of the string for each change we make to the string since String is immutable. We create a StringBuilder and that is mutable, so we can make all changes there and then make that a string and return it when done.
 
 //    // Method using StringBuilder API
 //    private static String reversedWithStringBuilder (String str){
@@ -21,15 +25,29 @@ public class ReverseStringRunner {
 //    }
 
 
-    // Reversing string manually without api
-        private static String reversedManually(String str) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = str.length() - 1; i >= 0; i--) {
-                sb.append(str.charAt(i));
-            }
-            return "str Reversed Manually is " + sb.toString();
 
+    //In this method we didn't use the reverse API. We did the reverse logic with a for loop, but we still used the StringBuilder so that we don't have to make new instances of the string and keep efficiency.
+    // Reversing string manually without api
+//        private static String reversedManually(String str) {
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = str.length() - 1; i >= 0; i--) {
+//                sb.append(str.charAt(i));
+//            }
+//            return "str Reversed Manually is " + sb.toString();
+//
+//        }
+
+
+
+    // In this next method we do it completely without using StringBuilder API. The issue with this is that it is the least efficient method since we make new instances for each iteration of the for loop.
+    private static String reverseStringManuallyWithoutStringBuilder(String str) {
+        String newStr = "";
+
+        for (int i = str.length() - 1 ; i >= 0 ; i--) {
+            newStr = newStr + str.charAt(i);
         }
+        return "Reversed  manually without StringBuilder - " + newStr;
+    }
 
 
 }
