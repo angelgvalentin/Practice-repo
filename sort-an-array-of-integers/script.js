bf4f7d1d5aa4467636ec3ec72e1cddba3cf4d260
Angel Valentin <angelgvalentin7@gmail.com>
2022-07-21 15:38:38 -0500

    added sorting array of integers escercise

---
+++ sort-an-array-of-integers/script.js
@@ -0,0 +1,32 @@
+let arr = [2, 4, 1, 6, 5, 8, 9, 3, 7];
+
+const sortAscending = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; i > j; j++) {
+            if (arr[i] < arr[j]) {
+                let x = arr[j];
+                arr[j] = arr[i];
+                arr[i] = x;
+            }
+        }
+    }
+
+    console.log("ascending array - " + arr);
+};
+
+const sortDescending = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; i > j; j++) {
+            if (arr[i] > arr[j]) {
+                let x = arr[j];
+                arr[j] = arr[i];
+                arr[i] = x;
+            }
+        }
+    }
+
+    console.log("descending array - " + arr);
+};
+
+sortAscending(arr);
+sortDescending(arr);
